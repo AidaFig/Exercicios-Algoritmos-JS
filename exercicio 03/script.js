dd0e44f18f620ecd65c9ccb29367a840e5ff50f9
AidaFig <aidafigueiredo8@gmail.com>
2021-05-24 17:29:45 -0300

    exercicios finalizados

--- exercicio 03/script.js
+++ exercicio 03/script.js
@@ -1,3 +1,14 @@
 //se a quantidade de maças for menor que uma duzia (12) o valor da unidade é 1,30
 //se a quantidade de maças for maior ou igual que 12 o valor da unidade é 1,00
 //o objetivo é calcular o valor da quantidade de macas compradas.
+
+function valorMacas (total) {
+    if (total < 12) {
+        let resultado = total * 1.30;
+        return `custo total da compra: R$${resultado}`
+    } else if (total >= 12) {
+        let resultado2 = total * 1
+        return `custo total da compra: R$${resultado2}`
+    }
+}
+console.log(valorMacas(9));
