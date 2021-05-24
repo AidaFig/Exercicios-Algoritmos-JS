bffe0f8e8354d5388cbd2c8d8f1237e5c13aef81
AidaFig <aidafigueiredo8@gmail.com>
2021-05-24 17:28:09 -0300

    exercicio 03 pronto

---
+++ exercicio 03/script.js
@@ -0,0 +1,3 @@
+//se a quantidade de maças for menor que uma duzia (12) o valor da unidade é 1,30
+//se a quantidade de maças for maior ou igual que 12 o valor da unidade é 1,00
+//o objetivo é calcular o valor da quantidade de macas compradas.
